817f797e23b1275a7a9646598ea46c026071eb07
camillalyons <33057608+camillalyons@users.noreply.github.com>
2018-01-29 16:10:54 +0100

    Update HWTask1.py
    The main idea of the program is: for a given word, the bk tree will find all the "similar" words, i.e. the ones with edit distance equal to 1. We then build an anytree that has as root the starting word and each node has as children the "similar" words,  so when we finally find v, the ending word, we can easilly find its absolute path.  In order to proceed through all the words in the dictionary from just one word, we created a list (words) to which we add , each time, all the new "similar" words.

--- HWTask1.py
+++ HWTask1.py
@@ -1,63 +1,67 @@
 from BKTree import BKtree, editDistanceFast
-from anytree import Node
+from anytree import Node, findall_by_attr
 
+#getting all the words from the dictionary
 dictionaryList = []
 with open('words.txt') as words:
     dictionary = words.readlines()
     for line in dictionary:
         line = line[:-2]
+        line.lower()
         dictionaryList.append(line)
 
+#from all the words from the dictionary, we are picking only the ones with the same length as the starting word (w)
 def selectingWords(w):
     rightWords = []
     for word in dictionaryList:
         if len(word) == len(w):
             rightWords.append(word)
-    return rightWords
+    return set(rightWords)
 
+#counting all the words of the right length
 def numberOfWordsFromDictionary(word):
-    c = 0
-    for el in dictionaryList:
-        if len(word) == len(el):
-            c += 1
-    return c
+    return (len(dictionaryList))
 
 
 def wordProblem(w,v):
+    output = []
     flag = 0
     dictList = selectingWords(w)
-    dictIter = iter(dictList)
-    tree = BKtree(dictIter, distance=editDistanceFast)
+    dictIter = iter(dictList)    #we need to make this an iterable for the bktree
+    tree = BKtree(dictIter, distance=editDistanceFast)    #for a given word, w, the bktree returnes all the words from the dictionary with edit distance equal to 1
 
-    c = numberOfWordsFromDictionary(w)
+    c = numberOfWordsFromDictionary(w)    #the number of words of the right length
 
-    words = tree.find(item = w, threshold = 1)
-
+    words = [w]    #takes count of all the words
     w = Node(w)
-    for el in words:
-        el = Node(el, parent = w)
+    wordsInTheTree = [w]    #this list takes count of all the words in the tree
 
     for ww in words:
-        if len(words) >= c:
+        if len(words) >= c:    #we have used up all the words from the dictoinary, so the 2 words are not equivalent
             return w, 'and', v, 'are not equivalent words'
-        newWords = tree.find(item = ww, threshold = 1)
-        ww = Node(ww)
+
+        newWords = tree.find(item = ww, threshold = 1)    #this list containes all the words of editdistance 1 from the starting word
+        father = findall_by_attr(w, ww)    #returnes a tuple with all the nodes that have the same name as the attribute we are seaching for
+        father = father[0]    #we only need 1 node: this will be the parent of all the words in "newWords"
         for el in newWords:
-            if flag == 0:
-                el = Node(el, parent = ww)
-                ww.parent = w
-                if el.name == v and flag == 0:
-                    return (el)
+            if flag == 0 and el not in wordsInTheTree:
+                el = Node(el, parent = father)
+                wordsInTheTree.append(str(el))
+                if el.name == v and flag == 0:    #so we have found the final word
+                    for x in el.path:    #appending all the nodes in the path of the final word
+                        output.append(x.name)
+                    for xx in output:
+                        print (xx, end = ' ')
                     flag = 1
-        if flag == 1:
+        if flag == 1:    #if we found the final word, we can break the program
             break
+
         for wo in newWords:
             if wo not in words:
                 words.append(wo)
 
 
+
 w = 'head'
 v = 'tell'
-print (wordProblem(w, v))
-
-
+wordProblem(w, v)
